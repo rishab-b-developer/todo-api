402384d9bd98ff5f212507992e65f1fb5b723745
Rishab Bokaria <rishab.bokaria@ril.com>
2017-06-19 13:21:35 +0530

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,30 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB Server: \n', JSON.stringify(err, undefined, 4));
+    }
+    console.log('Connected to MongoDB Server. \n');
+    /*db.collection('Todos').insertOne({
+        text: 'Test todo',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert todo: ', err);
+        }
+        console.log('Document inserted into todo: ', result);
+    })*/
+    /*db.collection('Users').insertOne({
+        name: 'Rishab Bokaria',
+        age: 27,
+        location: 'Mumbai'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert into Users: \n', JSON.stringify(err, undefined, 4));
+        }
+        console.log('Document inserted into Users: \n', JSON.stringify(result.ops, undefined, 4));
+        console.log(result.ops[0]._id.getTimestamp());
+    })*/
+
+    db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,32 @@
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB Server: \n', JSON.stringify(err, undefined, 4));
+    }
+    console.log('Connected to MongoDB Server. \n');
+    /* db.collection('Todos')
+         .find({
+             _id: new ObjectID('59477358e6e62f0615a88227')
+         })
+         .toArray()
+         .then((docs) => {
+             console.log('Todos');
+             console.log(JSON.stringify(docs, undefined, 4));
+         }, (err) => {
+             console.log('Unable to fetch documents:', JSON.stringify(err, undefined, 4));
+         });*/
+    db.collection('Users')
+        .find({ name: 'Rishab Bokaria' })
+        .toArray()
+        .then((docs) => {
+            console.log('Users');
+            console.log(JSON.stringify(docs, undefined, 4));
+        }, (err) => {
+            console.log('Unable to fetch documents:', JSON.stringify(err, undefined, 4));
+        });
+    db.close();
+});
