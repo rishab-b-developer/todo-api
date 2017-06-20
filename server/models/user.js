0b9ea9cfeb0827f210dcd02c829ac9a685344de0
Rishab Bokaria <rishab.bokaria@ril.com>
2017-06-20 16:21:07 +0530

    Added POST/users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -83,6 +83,25 @@ UserSchema.statics.findByToken = function(token) {
     });
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+    return User.findOne({ email })
+        .then((user) => {
+            if (!user) {
+                return Promise.reject({ error: 'email not registered!' });
+            }
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, user.password, (err, res) => {
+                    if (!res) {
+                        reject(err || { error: 'invalid password!' });
+                    } else {
+                        resolve(user);
+                    }
+                });
+            });
+        })
+}
+
 UserSchema.pre('save', function(next, done) {
     var user = this;
     if (user.isModified('password')) {
--- server/server.js
+++ server/server.js
@@ -54,6 +54,18 @@ app.post('/users', (request, response) => {
         .catch((err) => handleInvalidInputError(err, response));
 });
 
+app.post('/users/login', (request, response) => {
+    var body = _.pick(request.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password)
+        .then((user) => {
+            return user.generateAuthToken().then((token) => {
+                response.header('x-auth', token)
+                    .send(user);
+            });
+        })
+        .catch((err) => handleInvalidInputError(err, response));
+});
+
 app.get('/users/me', authenticate, (request, response) => {
     response.send(request.user);
 });
