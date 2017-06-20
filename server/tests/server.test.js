2ea289aeaea0a1a5e03b08ad8c773ab7b67c6ba8
Rishab Bokaria <rishab.bokaria@ril.com>
2017-06-20 17:42:24 +0530

    added test suite for DELETE/users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -372,5 +372,26 @@ describe('POST/users/login', () => {
                     .catch((e) => done(e));
             });
     });
+});
 
+describe('DELETE/users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app).delete('/users/me/token')
+            .set('x-auth', testUsers[0].tokens[0].token)
+            .expect(200)
+            .expect((response) => {
+                expect(response.body).toEqual({});
+            })
+            .end((err) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(testUsers[0]._id)
+                    .then((user) => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    })
+                    .catch((e) => done(e));
+            });
+    });
 });
